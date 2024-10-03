5d6e0df915837ca90c03acea53e3abcddf0af4c1
Thammaruthai <thammaruthai@gmail.com>
2024-10-03 13:30:09 +0700

    feat: Answer the ex1 and ex2

--- ex-1.js
+++ ex-1.js
@@ -1,19 +1,22 @@
 // Exercise #1: Strict and Same
 // Start commenting here
-Object.is(10, 20);
-Object.is(0, -0);
-Object.is(NaN, 100);
-Object.is("one", "two");
-Object.is({ name: "John", age: 30 }, { name: "John", age: 30 });
-Object.is(null, undefined);
+Object.is(10, 20); // false 
+Object.is(0, -0); // false
+Object.is(NaN, 100); // false
+Object.is("one", "two"); // false
+Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); // trueผิด เฉลยคือ false JavaScript objects ถูกเก็บเป็น references (ตัวชี้ตำแหน่งหน่วยความจำ) ไม่ใช่ค่าที่แท้จริง
+Object.is(null, undefined); // true ผิด เฉลยคือ false เพราะใน JavaScript null และ undefined เป็นค่าที่แตกต่างกัน แม้ว่าทั้งสองจะบ่งบอกถึงการไม่มีค่า แต่ความหมายทางเทคนิคของพวกมันแตกต่างกัน และ JavaScript จะไม่ถือว่ามันเท่ากัน
 
-10 === 20;
-10 === "10"
-3.14 !== 3.14;
-"TechUp" !== "TechUp";
-NaN === NaN;
-0 !== -0;
+10 === 20;  // false
+10 === "10" // false
+3.14 !== 3.14; // false
+"TechUp" !== "TechUp"; // false
+NaN === NaN; // false
+0 !== -0; // false
 
 let objectA = { course: "Web Developer", price: 300000 };
 let objectB = { course: "Web Developer", price: 300000 };
-objectA === objectB;
+objectA === objectB; // true ผิด เฉลยคือ false เพราะ จะตรวจสอบว่า object สองตัวนั้นอ้างอิง (reference) ถึงตำแหน่งเดียวกันในหน่วยความจำหรือไม่ ไม่ใช่ตรวจสอบว่าเนื้อหาข้างใน object นั้นเหมือนกันหรือไม่
+
+console.log(objectA === objectB);
+
